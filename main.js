c96742150a58bdabecd523db8d42b9ee6825e204
Tega Peter-Saduwa <tega.peter-saduwa@dipolediamond.com>
2019-07-19 09:25:29 +0100

    Electron proj

---
+++ main.js
@@ -0,0 +1,119 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+//set env
+process.env.NODE_ENV = 'production';
+
+let mainWindow;
+let addWindow;
+
+//Listen for app to be ready
+app.on('ready', function(){
+    //create new window
+    mainWindow = new BrowserWindow({
+        webPreferences: {nodeIntegration: true}
+    });
+    //load html into window
+    mainWindow.loadURL(url.format({
+        pathname:path.join(__dirname, 'mainWindow.html'),
+        protocol:'file:',
+        slashes:true
+    }));
+    //quit app when closed
+    mainWindow.on('closed', function (){
+        app.quit();
+    })
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //insert menu
+    Menu.setApplicationMenu(mainMenu);
+
+});
+//Handle create add window
+function createAddWindow() {
+        //create new window
+        addWindow = new BrowserWindow({
+            width:300,
+            height:200,
+            title:'Add Shopping List Item',
+            webPreferences: {nodeIntegration: true}
+            
+        });
+        //load html into window
+        addWindow.loadURL(url.format({
+        pathname:path.join(__dirname, 'addWindow.html'),
+        protocol:'file:',
+        slashes:true
+    }));
+    //Garbage collection handle
+    addWindow.on('close', function(){
+        addWindow = null;
+    });
+};
+
+//catch item:add
+ipcMain.on('item:add', function(e, item){
+    //send to mainwindow
+    //console.log(item);
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+})
+
+//create menu template
+const mainMenuTemplate = [
+
+    {
+        label: 'File',
+        submenu: [
+           { 
+               label: 'Add Item',
+               click(){
+                   createAddWindow();
+               }
+           },
+           {
+               label: 'Clear Items',
+               click(){
+                   mainWindow.webContents.send('item:clear');
+               }
+            },
+           {
+               label: 'Quit',
+               accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+               click () {
+                   app.quit();
+               }
+            }
+        ]
+    }
+];
+
+//if mac, add empty obj to menu
+if(process.platform== 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+//add devtools item if not in production
+if(process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I',
+                //focused window means the dev tools pop up on whatever window we are in
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+
+        ]
+    });
+}
